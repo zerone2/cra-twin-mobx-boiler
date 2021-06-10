8ccbef8784897bc901997e0f48fd129c4570d99b
zerone2 <yicho93@naver.com>
2021-06-10 18:53:28 +0900

    [add] add material ui

--- src/pages/SamplePage.js
+++ src/pages/SamplePage.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
+import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers'
+import DayjsUtils from '@date-io/dayjs'
 
 const SamplePage = () => {
   const history = useHistory()
+  const [selectedDate, handleDateChange] = useState(new Date())
   return (
-    <div className='App'>
-      <h1>React Sample Page</h1>
-      <h3>url = {history.location.pathname}</h3>
-    </div>
+    <MuiPickersUtilsProvider utils={DayjsUtils}>
+      <div className='App'>
+        <h1>Calendar sample page</h1>
+        {/*<h3>url = {history.location.pathname}</h3>*/}
+        <div style={{ display: 'flex', flex: 'row nowrap', justifyContent: 'center', alignItems: 'center', marginTop: '30px' }}>
+          <div style={{ marginRight: '10px' }}>Calendar: </div>
+          <DatePicker value={selectedDate} onChange={handleDateChange} />
+        </div>
+      </div>
+    </MuiPickersUtilsProvider>
   )
 }
 
